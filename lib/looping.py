b0d61153691dd33cd07338b2b878a626efb1cc42
+Sam Thongo <+samuelthongo73@gmail.com>
2023-10-04 00:31:04 +0300

    looping

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    counter = 10
+    while (counter > 0):
+        counter -= 1
+        print(counter)
+    print("Happy New Year!")
     pass
 
+
 def square_integers(int_list):
-    # code goes here!
+    int_list = [i**2 for i in int_list]
+    print(int_list)
     pass
 
 def fizzbuzz():
-    # code goes here!
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
     pass
